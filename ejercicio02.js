61c3c32ced5eaf8191b139b01769abcc6e5f5bfc
AndersonJimenez <anjimhol23@gmail.com>
2023-05-29 21:20:58 -0500

    Actualizacion ejercicio #1 numero palíndromo

---
+++ ejercicio02.js
@@ -0,0 +1,27 @@
+function tel(number) {
+let numString = number.toString()
+console.log( numString)
+
+if (numString.length > 10){
+console.log("valor errado")
+
+return numString
+
+} else console.log( "correcto" + numString)
+
+}
+
+
+
+let telefono = 3166173440
+let numTel = tel (telefono)
+
+
+/*let numero = "3166173440"
+
+if (numero.length > 10){
+    console.log("valor errado")
+
+} else {
+    console.log("CORRECTO")
+}*/
